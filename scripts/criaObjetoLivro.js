2552d31e463fcdbc96413c2dbe232c2c22a860a6
João Filipe <joaofilipe8906@gmail.com>
2023-11-29 15:05:36 -0300

    Projeto Finalizado(Apenas falta as animações)

---
+++ scripts/criaObjetoLivro.js
@@ -0,0 +1,34 @@
+export default function criaCardLivro(livro){
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = `
+        <picture class="capa">
+            <img src="${livro.imagem}" alt="Capa do livro">
+        </picture>
+        <div class="infoLivro">
+            <h1>${livro.titulo}</h1>
+            <div>
+            <h2>Autor:</h2>
+            <p>${livro.autor}</p>
+            </div>
+            <div>
+                <h2>Editora:</h2>
+                <p>${livro.editora}</p>
+            </div>
+            <div class="Publinas">
+                <div>
+                    <h2>Publicado:</h2>
+                    <p>${livro.publicado}</p>
+                </div>
+                <div>
+                    <h2>Páginas:</h2>
+                    <p>${livro.paginas}</p>
+                </div>
+            </div>
+            <a href="${livro.compra}"><button class="compraLivro">COMPRAR</button></a>
+        </div>`
+        
+    const main = document.querySelector('main')
+    main.classList.add('main')
+    return card
+}
